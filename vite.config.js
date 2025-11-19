977768c667bd8ffc71539bca19f0cf1f3bbb7609
Pruthvi070 <chavanpruthviraj9595@gmail.com>
2025-11-19 18:16:38 +0530

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",   // <── THIS FIXES IMAGE + LOGO ISSUE
+  base: "./",   // Required for Netlify to load images correctly
 })
